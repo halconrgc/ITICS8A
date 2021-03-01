59f373a2c7b7247bd843dc3d7a2c59ecf7251d2c
halconrgc <31252541+halconrgc@users.noreply.github.com>
2021-03-01 07:47:15 -0600

    Update MenuOp.java
    Ya cree el case 1 promedio de 3 calificaciones dentro de la clase main

--- src/menuop/MenuOp.java
+++ src/menuop/MenuOp.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 public class MenuOp {
     public static void main(String[] args) {
        int opcion=0;
+       int calf1,calf2,calf3,promedio; 
        do {
            opcion=Integer.parseInt(JOptionPane.showInputDialog("<<<<<<<<<<<<<<<<<<<menu>>>>>>>>>>>>>>>>>"
                    + "\n1.-promedio de 3 calificaciones"
@@ -13,28 +14,25 @@ public class MenuOp {
           switch(opcion){
               case 1:
                   JOptionPane.showMessageDialog(null, "Promedio");
+                  calf1=Integer.parseInt(JOptionPane.showInputDialog("Dame la primer calificación"));
+                  calf2=Integer.parseInt(JOptionPane.showInputDialog("Dame la segunda calificación"));
+                  calf3=Integer.parseInt(JOptionPane.showInputDialog("Dame la última calificación"));
+                  promedio=(calif1+calif2+calif3)/3;
+                  JOptionPane.showMessageDialog(null, "El Promedio es: "+promedio);
                   break;
               case 2:
                   JOptionPane.showMessageDialog(null, "Tabla");
-
                   break;
-                  
               case 3:
                   JOptionPane.showMessageDialog(null, "serie");
-
                   break;
-                  
               case 4:
                 JOptionPane.showMessageDialog(null, "Adios");
-
                   break;
-                  
               default:
                 JOptionPane.showMessageDialog(null, "Opcion no valida");
-
                   break;
           }
-           
        }while(opcion != 4);
     }
 }
